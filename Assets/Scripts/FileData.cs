944013b7f9415622ea34c988a287fbd9b6f6beff
lead8964878 <lead8964878@yahoo.com.tw>
2019-10-08 19:34:24 +0800

    Add FileData.cs

---
+++ Assets/Scripts/FileData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[Serializable]
+public struct BezierPoint
+{
+    public Vector3 up, down, ori;
+
+    public BezierPoint(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        up = v1;
+        down = v2;
+        ori = v3;
+    }
+    public void SetOri(Vector3 v) {
+        ori = v;
+    }
+};
+
+[Serializable]
+public class PathData {
+    public int _pointLength;
+    public List<BezierPoint> _ctrlPoint;
+
+    public PathData(List<BezierPoint> ctrlPoint) {
+        _pointLength = ctrlPoint.Count;
+        _ctrlPoint = ctrlPoint;
+    }
+}
